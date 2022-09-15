4de962dbf62adcfcf457ee379b5a17e805ab9a1b
LambertKlink <lambert.klink@stud.htwk-leipzig.de>
2022-09-15 17:24:26 +0100

    makes checker work on first click

--- tutorial/assets/js/checker.js
+++ tutorial/assets/js/checker.js
@@ -1,32 +1,37 @@
 var responseUserInput = "";
+var once = false;
 const yasgui = new Yasgui(document.getElementById("yasgui"));
 var responseRightAnswer;
+fetch('https://dbpedia.org/sparql', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/sparql-results+json,*/*;q=0.9',
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Length': '250'
+        },
+        body: encodeURI(rightAnswerQuery)
+    })
+    .then((response) => response.json())
+    .then((data) => responseRightAnswer = data);
+
+
+
 yasgui.on("query", () => {
-
-    fetch('https://dbpedia.org/sparql', {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/sparql-results+json,*/*;q=0.9',
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'Content-Length': '250'
-            },
-            body: encodeURI(rightAnswerQuery)
-        })
-        .then((response) => response.json())
-        .then((data) => responseRightAnswer = data);
-
     //renames variables in answer to different values, so that user can choose their own variable name
-    //responseRightAnswer is not always defined on the first click as waiting for the HTTP Request takes some time
     //Variable names are not allowed to be named after array values!!!
+
     if (responseRightAnswer !== undefined) {
         var head = responseRightAnswer.head;
         var results = responseRightAnswer.results;
-        for (i = 0; i < head.vars.length; i++) {
-            let name = head.vars[i];
-            let array = ['972', '973', '974', '975', '976', '977', '978', '979'];
-            head.vars[i] = array[i];
-            results.bindings[0][array[i]] = results.bindings[0][name];
-            delete results.bindings[0][name];
+        if (once == false) {
+            for (i = 0; i < head.vars.length; i++) {
+                let name = head.vars[i];
+                let array = ['972', '973', '974', '975', '976', '977', '978', '979'];
+                head.vars[i] = array[i];
+                results.bindings[0][array[i]] = results.bindings[0][name];
+                delete results.bindings[0][name];
+            }
+            once = true;
         }
         responseRightAnswer.results = results;
         responseRightAnswer.head = head;
